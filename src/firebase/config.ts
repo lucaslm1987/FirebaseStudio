873fa5d63f8add7f344b0ab96622b8691ce024c5
Lucas Masters Tips & Gameplay <lucas.lmasters@gmail.com>
2025-11-10 10:58:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1550828537-2e6a0",
+  "appId": "1:32630749561:web:ab97cff5ed597c041bd7a8",
+  "apiKey": "AIzaSyDLeaUK4iwd8F0GCSbbTal13KKNZajuwaY",
+  "authDomain": "studio-1550828537-2e6a0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "32630749561"
+};
